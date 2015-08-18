a0843c3a16837f37f7db118a0a3815bc18470b56
nadiadev <creation.nadia@gmail.com>
2015-08-18 15:01:52 +0200

    add pages error/404

---
+++ app/templates/error_layout.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta charset="UTF-8">
+	<title><?= $this->e($title) ?></title>
+
+	
+</head>
+<body>
+	
+			<?= $this->section('main_content') ?>
+		
+</body>
+</html>
--- app/templates/w_errors/404.php
+++ app/templates/w_errors/404.php
@@ -1,4 +1,4 @@
-<?php $this->layout('layout', ['title' => 'Perdu ?']) ?>
+<?php $this->layout('error_layout', ['title' => 'Perdu ?']) ?>
 
 <?php $this->start('main_content'); ?>
 <h1>404. Perdu ?</h1>
